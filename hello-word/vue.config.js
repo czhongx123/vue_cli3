e87a1256395df930413d5b3f3144e89488ce07ae
czhongx123 <792006914@qq.com>
2018-12-14 18:04:03 +0800

    添加项目

--- hello-word/vue.config.js
+++ hello-word/vue.config.js
@@ -4,7 +4,10 @@ module.exports={
     },
     configureWebpack:{
 
-    },
+    },//该对象将会被 webpack-merge 合并入最终的 webpack 配置。
+    pages:{
+
+    }//用于配置多页面应用
     // devServer:{
     //     host:'loacalhost',
     //     post:'8080'
