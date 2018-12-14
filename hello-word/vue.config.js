a06ec6a5295f96c7083fe9c0f98dddf187d22321
czhongx123 <792006914@qq.com>
2018-12-14 15:30:32 +0800

    重建构建项目

---
+++ hello-word/vue.config.js
@@ -0,0 +1,13 @@
+module.exports={
+    css:{
+        extract:false//是否使用css分离插件
+    },
+    configureWebpack:{
+
+    },
+    // devServer:{
+    //     host:'loacalhost',
+    //     post:'8080'
+    // }
+}
+
